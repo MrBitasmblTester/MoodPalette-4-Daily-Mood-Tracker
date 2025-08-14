eb766b61b6f54f7b6cc59dd4ae36d00ac1b8024d
MrBitasmblTester <21100302@ibsu.edu.ge>
2025-08-14 16:35:40 +0400

    Add CalendarHeatmap.js

---
+++ src/components/CalendarHeatmap.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function CalendarHeatmap({data}) {
+  return (
+    <div className="heatmap">
+      {/* Iterate days and render divs colored by data[date] */}
+    </div>
+  );
+}
